f45b8be4c5c4d78e71a7b3191512621221a01d36
Sheikh-003 <noughtysheikh303@gmail.com>
2024-05-31 18:45:27 +0500

    Add files via upload

---
+++ GUI_calculator/main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.header('This is our calculator')
+st.title("This is a title")
+st.subheader("This is a subheader")
+st.text("Hello this is text")
+st.markdown("##### This is a markdown")
+st.success("Success message")
+st.info("Information")
+
+# success
+st.warning("Warning")
+
+# success
+st.error("Error")
